4320db2aa53f6c1164db218871bfe9747e758617
Debasish Besra <debasish.besra.ai@gmail.com>
2026-02-02 09:44:08 +0530

    data ingestion added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "PulseTube",
+    version = "1.0.0",
+    author = "Debasish Besra",
+    author_email = "debasish.besra.ai@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
